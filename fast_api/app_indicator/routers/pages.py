d5419de63a1b1e146085a6612ac8f0322d637866
Federico Stachurski <rico.stachurski@hotmail.com>
2026-09-09 14:39:34 +0100

    [debug/deploy] changed pages.py: Fix Jinja TemplateResponse syntax

--- fast_api/app_indicator/routers/pages.py
+++ fast_api/app_indicator/routers/pages.py
@@ -3,16 +3,30 @@ from __future__ import annotations
 from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+
 from ..core.config import TEMPLATES_DIR
 
 
 router = APIRouter(tags=["pages"])
-templates = Jinja2Templates(directory=str(TEMPLATES_DIR))
+
+templates = Jinja2Templates(
+    directory=str(TEMPLATES_DIR)
+)
+
 
 @router.get("/", response_class=HTMLResponse)
 def index(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    return templates.TemplateResponse(
+        request=request,
+        name="index.html",
+        context={},
+    )
+
 
 @router.get("/paper", response_class=HTMLResponse)
 def paper(request: Request):
-    return templates.TemplateResponse("paper.html", {"request": request})
+    return templates.TemplateResponse(
+        request=request,
+        name="paper.html",
+        context={},
+    )
